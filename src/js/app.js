b9ef9cfaf70e6ac05076b7acd553726d7ad307b6
Takashi Kitajima <inc@2inc.org>
2016-08-18 09:31:00 +0900

    Replace drawer

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import BasisDrawer from '../../node_modules/sass-basis-drawer/src/js/drawer.js';
+import BasisDrawer from '../../node_modules/getbasis-drawer/src/js/drawer.js';
 import BasisFixedHeader from '../../node_modules/getbasis-layout/src/js/fixed-header.js';
 import BasisOverlayHeader from '../../node_modules/getbasis-layout/src/js/overlay-header.js';
 import BasisMenu from '../../node_modules/sass-basis-menu/src/js/menu.js';
