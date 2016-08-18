fa4caade8869ac1d0d67dcda09a80b7f1c05c760
Takashi Kitajima <inc@2inc.org>
2016-08-18 10:01:06 +0900

    Replace menu

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@
 import BasisDrawer from '../../node_modules/getbasis-drawer/src/js/drawer.js';
 import BasisFixedHeader from '../../node_modules/getbasis-layout/src/js/fixed-header.js';
 import BasisOverlayHeader from '../../node_modules/getbasis-layout/src/js/overlay-header.js';
-import BasisMenu from '../../node_modules/sass-basis-menu/src/js/menu.js';
+import BasisMenu from '../../node_modules/getbasis-menu/src/js/menu.js';
 
 new BasisDrawer();
 new BasisFixedHeader();
