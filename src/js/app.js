9a7cfd5c0579b242fdec47d895d5455907e89a73
Takashi Kitajima <inc@2inc.org>
2016-11-02 11:07:24 +0900

    Refactoring drawer nav

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,12 @@ import BasisFixedHeader from '../../node_modules/getbasis-layout/src/js/fixed-he
 import BasisOverlayHeader from '../../node_modules/getbasis-layout/src/js/overlay-header.js';
 import BasisMenu from '../../node_modules/getbasis-menu/src/js/menu.js';
 
-new BasisDrawer();
+new BasisDrawer({
+  container    : '.p-drawer-nav',
+  drawer       : '.p-drawer-nav__body',
+  btn          : '.p-drawer-nav__btn',
+  toggleSubmenu: '.p-drawer-nav__toggle'
+});
 new BasisFixedHeader();
 new BasisOverlayHeader();
 new BasisMenu();
