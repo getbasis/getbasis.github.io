5a4b1c94bc7eef710b37324219b942e0bc3ce0f0
Takashi Kitajima <inc@2inc.org>
2016-11-25 15:09:53 +0900

    Added loading effect

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,15 @@
 'use strict';
 
+import $ from 'jquery';
+
 import BasisDrawer from '../../node_modules/getbasis-drawer/src/js/drawer.js';
 import BasisFixedHeader from '../../node_modules/getbasis-layout/src/js/fixed-header.js';
 import BasisStickyHeader from '../../node_modules/getbasis-layout/src/js/sticky-header.js';
 import BasisMenu from '../../node_modules/getbasis-menu/src/js/menu.js';
+import BasisLoading from './loading.js';
 
 new BasisDrawer();
 new BasisFixedHeader();
 new BasisStickyHeader();
 new BasisMenu();
+new BasisLoading();
