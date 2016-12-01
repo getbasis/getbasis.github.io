4e1875f507a573b82d585796942508257f2b8c28
Takashi Kitajima <inc@2inc.org>
2016-12-01 14:57:24 +0900

    Added page-effect component

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ import BasisDrawer from '../../node_modules/getbasis/src/js/drawer.js';
 import BasisFixedHeader from '../../node_modules/getbasis-layout/src/js/fixed-header.js';
 import BasisStickyHeader from '../../node_modules/getbasis-layout/src/js/sticky-header.js';
 import BasisMenu from '../../node_modules/getbasis-menu/src/js/menu.js';
-import BasisPageEffect from '../../node_modules/getbasis-page-effect/src/js/page-effect.js';
+import BasisPageEffect from '../../node_modules/getbasis/src/js/page-effect.js';
 
 new BasisDrawer();
 new BasisFixedHeader();
