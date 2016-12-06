9e935f61bd0371fa835157a187269fb991f71ea4
Takashi Kitajima <inc@2inc.org>
2016-12-06 14:47:47 +0900

    Added navbar components

--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,11 @@ import $ from 'jquery';
 import BasisDrawer from '../../node_modules/getbasis/src/js/drawer.js';
 import BasisFixedHeader from '../../node_modules/getbasis-layout/src/js/fixed-header.js';
 import BasisStickyHeader from '../../node_modules/getbasis-layout/src/js/sticky-header.js';
-import BasisMenu from '../../node_modules/getbasis-menu/src/js/menu.js';
+import BasisNavbar from '../../node_modules/getbasis/src/js/navbar.js';
 import BasisPageEffect from '../../node_modules/getbasis/src/js/page-effect.js';
 
 new BasisDrawer();
 new BasisFixedHeader();
 new BasisStickyHeader();
-new BasisMenu();
+new BasisNavbar();
 new BasisPageEffect();
