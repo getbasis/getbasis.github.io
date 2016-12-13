5d6e1e391b47aee0e138460f00170d9a73a4193b
Takashi Kitajima <inc@2inc.org>
2016-12-13 19:26:25 +0900

    Update Basis version

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,15 @@ var rename       = require('gulp-rename');
 var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano      = require('cssnano');
-var browserify   = require('browserify');
-var source       = require('vinyl-source-stream');
-var buffer       = require('vinyl-buffer');
-var babelify     = require('babelify');
 var browser_sync = require('browser-sync');
 var ejs          = require('gulp-ejs');
+var uglify       = require('gulp-uglify');
+var rollup       = require('gulp-rollup');
+var nodeResolve  = require('rollup-plugin-node-resolve');
+var commonjs     = require('rollup-plugin-commonjs');
+var babel        = require('rollup-plugin-babel');
 
-var path = {
+var dir = {
   src: {
     css    : 'src/stylus',
     js     : 'src/js',
@@ -39,46 +40,60 @@ var path = {
 /**
  * ES6 to ES5
  */
+
 gulp.task('js', function() {
-  return browserify({
-    entries: 'src/js/app.js'
-  })
-  .transform('babelify', {presets: ['es2015']})
-  .bundle()
-  .pipe(source('app.js'))
-  .pipe(buffer())
-  .pipe(gulp.dest(path.dist.js));
-} );
+  gulp.src(dir.src.js + '/**/*.js')
+    .pipe(rollup({
+      allowRealFiles: true,
+      entry: dir.src.js + '/app.js',
+      format: 'iife',
+      plugins: [
+        nodeResolve({ jsnext: true }),
+        commonjs(),
+        babel({
+          presets: ['es2015-rollup'],
+          babelrc: false
+        })
+      ]
+    }))
+    .pipe(gulp.dest(dir.dist.js))
+    .on('end', function() {
+      gulp.src([dir.dist.js + '/app.js'])
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(dir.dist.js));
+    });
+});
 
 /**
  * Stylus to CSS
  */
 gulp.task('css', function() {
-  return gulp.src(path.src.css + '/*.styl')
+  return gulp.src(dir.src.css + '/*.styl')
     .pipe(stylus({
       'include css': true
     }))
-    .pipe(gulp.dest(path.dist.css))
+    .pipe(gulp.dest(dir.dist.css))
     .pipe(postcss([autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     })]))
-    .pipe(gulp.dest(path.dist.css))
+    .pipe(gulp.dest(dir.dist.css))
     .pipe(postcss([cssnano()]))
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest(path.dist.css));
+    .pipe(gulp.dest(dir.dist.css));
 });
 
 /**
  * EJS to HTML
  */
 gulp.task('ejs', function() {
-  gulp.src(path.src.ejs)
+  gulp.src(dir.src.ejs)
   .pipe(ejs(
     {
-      version: '4.1.0',
+      version: '4.2.0',
       css    : '/assets/css',
       js     : '/assets/js',
       images : '/assets/images',
@@ -86,29 +101,29 @@ gulp.task('ejs', function() {
     },
     {ext: '.html'})
   )
-  .pipe(gulp.dest(path.dist.ejs));
+  .pipe(gulp.dest(dir.dist.ejs));
 });
 
 /**
  * images
  */
 gulp.task( 'imagecopy', function(){
-  gulp.src(path.src.images + '/**/*.+(jpg|jpeg|png|gif|svg)')
-    .pipe(gulp.dest(path.dist.images));
+  gulp.src(dir.src.images + '/**/*.+(jpg|jpeg|png|gif|svg)')
+    .pipe(gulp.dest(dir.dist.images));
 } );
 gulp.task('favicon', function(){
-  gulp.src(path.src.favicon)
-    .pipe(gulp.dest(path.dist.favicon));
+  gulp.src(dir.src.favicon)
+    .pipe(gulp.dest(dir.dist.favicon));
 } );
 
 /**
  * Auto Compile.
  */
 gulp.task('watch', function() {
-  gulp.watch([path.src.css + '/**/*.styl'], ['css']);
-  gulp.watch([path.src.js + '/**.js'], ['js']);
-  gulp.watch([path.src.images + '/**/*.+(jpg|jpeg|png|gif|svg)'], ['imagecopy']);
-  gulp.watch([path.src.favicon], ['favicon']);
+  gulp.watch([dir.src.css + '/**/*.styl'], ['css']);
+  gulp.watch([dir.src.js + '/**.js'], ['js']);
+  gulp.watch([dir.src.images + '/**/*.+(jpg|jpeg|png|gif|svg)'], ['imagecopy']);
+  gulp.watch([dir.src.favicon], ['favicon']);
   gulp.watch(['src/ejs/**/*.ejs'], ['ejs']);
 });
 
--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,9 @@
 
 import $ from 'jquery';
 
-import BasisDrawer from '../../node_modules/getbasis/src/js/drawer.js';
+import '../../node_modules/getbasis/src/js/basis.js';
 import BasisFixedHeader from '../../node_modules/getbasis-layout/src/js/fixed-header.js';
 import BasisStickyHeader from '../../node_modules/getbasis-layout/src/js/sticky-header.js';
-import BasisNavbar from '../../node_modules/getbasis/src/js/navbar.js';
-import BasisPageEffect from '../../node_modules/getbasis/src/js/page-effect.js';
 
-new BasisDrawer();
 new BasisFixedHeader();
 new BasisStickyHeader();
-new BasisNavbar();
-new BasisPageEffect();
